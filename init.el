c02d9d7fbf55fe6f9c4f7d0891cafd2e07b5b717
120267583@qq.com <120267583@qq.com>
2017-07-11 17:11:16 +0800

    init

---
+++ init.el
@@ -0,0 +1,42 @@
+;;隐藏工具栏
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(tool-bar-mode -1)
+
+;;隐藏滚动条
+(scroll-bar-mode -1)
+
+;;设置缩进
+(electric-indent-mode -1)
+
+(setq inhibit-splash-srceen t)
+
+;;显示行号
+(linum-mode t)
+
+;;打开配置文件的函数
+(defun open-init-file ()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+;;绑定快捷键f1 为打开配置文件
+(global-set-key (kbd "<f1>")
+		'open-init-file)
+
+;;设置合适字体
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family #("微软雅黑" 0 4 (charset chinese-gbk)) :foundry "outline" :slant normal :weight normal :height 120 :width normal)))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (company))))
