d23203c499c3db97f6f9c7a1d7f6e262ec1cb438
120267583@qq.com <120267583@qq.com>
2017-07-11 19:37:20 +0800

    安装新主题

--- init.el
+++ init.el
@@ -1,11 +1,20 @@
 
 
 
+
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
+
+
+
+;;设置新的插件源
+(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
+                    (not (gnutls-available-p))))
+       (url (concat (if no-ssl "http" "https") "://melpa.org/packages/")))
+  (add-to-list 'package-archives (cons "melpa" url) t))
 
 
 
@@ -44,6 +53,9 @@
 ;;自动高亮光标所在的行
 (global-hl-line-mode)
 
+;;加载新主题
+(load-theme 'monokai t)
+
 ;;设置合适字体
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -56,4 +68,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (company))))
+ '(custom-safe-themes
+   (quote
+    ("a49760e39bd7d7876c94ee4bf483760e064002830a63e24c2842a536c6a52756" default)))
+ '(package-selected-packages (quote (monokai-theme company))))
