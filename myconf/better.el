b07613b955cb07cc39fca2bddb274df55cdabd54
120267583@qq.com <120267583@qq.com>
2017-07-12 13:48:32 +0800

    调整配置文件结构

--- myconf/better.el
+++ myconf/better.el
@@ -4,3 +4,37 @@
 ;;禁止自动备份文件
 (setq auto-save-default nil)
 (setq make-backup-files nil)
+
+;;启动自动补全
+(global-company-mode)
+
+;;增强字符删除功能
+(require 'hungry-delete)
+(global-hungry-delete-mode)
+
+;;开启mini buffer增强功能插件
+(ivy-mode 1)
+(setq ivy-use-virtual-buffers t)
+
+;;配置增强版本M-x
+(global-set-key (kbd "M-x")
+		'counsel-M-x)
+
+;;配置增强版本搜索
+(global-set-key (kbd "\C-s")
+		'swiper)
+
+;;配置增强版文件查找
+(global-set-key (kbd "C-x C-f")
+		'counsel-find-file)
+
+;;设置自动补全插件出现时间
+(setq-default company-idle-delay 0.01)
+
+;;设置左右括号自动补齐
+(smartparens-global-mode t)
+
+;;增强窗口切换
+(require 'popwin)
+(popwin-mode t)
+
--- myconf/main.el
+++ myconf/main.el
@@ -10,42 +10,24 @@
 ;;修改emacs默认行为
 (load-file "~/.emacs.d/myconf/better.el")
 
-;;启动自动补全
-(global-company-mode)
+
+;;定义批量加载函数
+(defun load-files (files dir)
+  (if  (> (length files) 0)
+      (progn
+	(load-file (concat dir (car files)))
+	(load-files (cdr files)
+		    dir ))))
+;;批量加载插件配置
+(load-files (cdr (cdr
+		  (directory-files
+		   "~/.emacs.d/myconf/addon")))
+	    "~/.emacs.d/myconf/addon/")
 
 ;;启动elisp括号匹配
 (add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
 
-;;安装空格删除插件
-(require 'hungry-delete)
-(global-hungry-delete-mode)
-
-;;开启增强功能插件
-(ivy-mode 1)
-(setq ivy-use-virtual-buffers t)
-
-;;配置增强版本M-x
-(global-set-key (kbd "M-x")
-		'counsel-M-x)
-
-;;配置增强版本搜索
-(global-set-key (kbd "\C-s")
-		'swiper)
-
-;;配置增强版文件查找
-(global-set-key (kbd "C-x C-f")
-		'counsel-find-file)
-
-;;设置自动补全时间
-(setq-default company-idle-delay 0.01)
-
-;;设置左右括号自动补齐
-(smartparens-global-mode t)
 
 
 
 
-
-;;增强窗口切换
-(require 'popwin)
-(popwin-mode t)
