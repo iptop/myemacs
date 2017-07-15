adf7662eeab779da31a137a50cadc8de2500d6b0
120267583@qq.com <120267583@qq.com>
2017-07-15 22:29:21 +0800

    1、安装js2-mode 2、绑定宏录制和播放快捷键

--- myconf/key.el
+++ myconf/key.el
@@ -6,3 +6,16 @@
 ;;绑定f2打开插件列表
 (global-set-key (kbd "<f2>")
 		'open-pkg-list)
+
+
+;;绑定f11为开始录制宏
+(global-set-key (kbd "<f11>")
+		'kmacro-start-macro)
+
+;;绑定f12为结束录制宏
+(global-set-key (kbd "<f12>")
+		'kmacro-end-macro)
+
+;;绑定快捷键f10为播放宏
+(global-set-key (kbd "<f10>")
+		'kmacro-end-and-call-macro)
